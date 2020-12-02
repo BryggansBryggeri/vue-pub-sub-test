a382a6d7589aa0e16e30c331cf89d8ddf72d0335
jonathanFager <faaager@gmail.com>
2020-12-02 16:42:17 +0100

    Added design of three different status cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,12 +21,12 @@
       <div
         class="w-full mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pt-6 gap-8"
       >
-        <div class="rounded shadow-lg border">
-          <div class="mx-auto bg-white shadow rounded w-full">
+        <div class="rounded shadow-lg border border">
+          <div class="mx-auto shadow rounded w-full">
             <div
-              class="flex flex-row bg-gray-100 rounded-t-md justify-between w-full shadow-sm mb-2"
+              class="flex flex-row bg-gray-200 bg-opacity-40 rounded-t justify-between w-full shadow-sm mb-2 px-4 py-2"
             >
-              <div class="sm:w-3/5 mb-4 sm:mb-0 p-2">
+              <div class="sm:w-3/5 mb-4 sm:mb-0">
                 <h2 class="text-gray-800 text-lg font-bold">
                   { { Vessel.Name } }
                 </h2>
@@ -37,56 +37,18 @@
                   Main actor: { { vessel.actor } }
                 </p>
               </div>
-              <div class="flex">
-                <div class="mr-1 mt-1 xl:mb-0 text-red-500">
+              <div
+                class="flex text-green-600 rounded-tr"
+              >
+                <div class="my-auto mx-auto">
                   <svg
-                    class="h-6 w-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
-                    />
-                  </svg>
-                </div>
-                <div class="mr-1 mt-1 xl:mb-0 text-yellow-500">
-                  <svg
-                    class="h-6 w-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0v-4a1 1 0 0 1 1-1zm0-4a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-                    />
-                  </svg>
-                </div>
-                <div class="mr-1 mt-1 xl:mb-0 text-green-500">
-                  <svg
-                    class="h-6 w-6 fill-current"
+                    class="h-8 w-8 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                   >
                     <path
                       class="heroicon-ui"
                       d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"
-                    />
-                  </svg>
-                </div>
-              </div>
-              <div
-                class="flex bg-red-500 px-4 border-l-4 border-gray-300 text-white rounded-tr-lg"
-              >
-                <div class="my-auto mx-auto">
-                  <svg
-                    class="h-6 w-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
                     />
                   </svg>
                 </div>
@@ -119,12 +81,12 @@
           </div>
         </div>
 
-        <div class="rounded shadow-lg border">
-          <div class="mx-auto bg-white shadow rounded w-full">
+        <div class="rounded shadow-lg border border-yellow-500">
+          <div class="mx-auto bg-yellow-50 shadow rounded w-full">
             <div
-              class="flex flex-row bg-gray-100 rounded-t-md justify-between w-full shadow-sm mb-2"
+              class="flex flex-row bg-gray-200 bg-opacity-40 rounded-t justify-between w-full shadow-sm mb-2 px-4 py-2"
             >
-              <div class="sm:w-3/5 mb-4 sm:mb-0 p-2">
+              <div class="sm:w-3/5 mb-4 sm:mb-0">
                 <h2 class="text-gray-800 text-lg font-bold">
                   { { Vessel.Name } }
                 </h2>
@@ -135,56 +97,16 @@
                   Main actor: { { vessel.actor } }
                 </p>
               </div>
-              <div class="flex">
-                <div class="mr-1 mt-1 xl:mb-0 text-red-500">
+              <div class="flex text-yellow-600 rounded-tr">
+                <div class="my-auto mx-auto">
                   <svg
-                    class="h-6 w-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
-                    />
-                  </svg>
-                </div>
-                <div class="mr-1 mt-1 xl:mb-0 text-yellow-500">
-                  <svg
-                    class="h-6 w-6 fill-current"
+                    class="h-8 w-8 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                   >
                     <path
                       class="heroicon-ui"
                       d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0v-4a1 1 0 0 1 1-1zm0-4a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-                    />
-                  </svg>
-                </div>
-                <div class="mr-1 mt-1 xl:mb-0 text-green-500">
-                  <svg
-                    class="h-6 w-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"
-                    />
-                  </svg>
-                </div>
-              </div>
-              <div
-                class="flex bg-red-500 px-4 border-l-4 border-gray-300 text-white rounded-tr-lg"
-              >
-                <div class="my-auto mx-auto">
-                  <svg
-                    class="h-6 w-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
                     />
                   </svg>
                 </div>
@@ -217,12 +139,12 @@
           </div>
         </div>
 
-                <div class="rounded shadow-lg border">
-          <div class="mx-auto bg-white shadow rounded w-full">
+        <div class="rounded shadow-lg border border-red-500">
+          <div class="mx-auto bg-red-100 shadow rounded w-full">
             <div
-              class="flex flex-row bg-gray-100 rounded-t-md justify-between w-full shadow-sm mb-2"
+              class="flex flex-row bg-gray-200 bg-opacity-40 rounded-t justify-between w-full shadow-sm mb-2 px-4 py-2"
             >
-              <div class="sm:w-3/5 mb-4 sm:mb-0 p-2">
+              <div class="sm:w-3/5 mb-4 sm:mb-0">
                 <h2 class="text-gray-800 text-lg font-bold">
                   { { Vessel.Name } }
                 </h2>
@@ -233,50 +155,12 @@
                   Main actor: { { vessel.actor } }
                 </p>
               </div>
-              <div class="flex">
-                <div class="mr-1 mt-1 xl:mb-0 text-red-500">
-                  <svg
-                    class="h-6 w-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
-                    />
-                  </svg>
-                </div>
-                <div class="mr-1 mt-1 xl:mb-0 text-yellow-500">
-                  <svg
-                    class="h-6 w-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0v-4a1 1 0 0 1 1-1zm0-4a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-                    />
-                  </svg>
-                </div>
-                <div class="mr-1 mt-1 xl:mb-0 text-green-500">
-                  <svg
-                    class="h-6 w-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"
-                    />
-                  </svg>
-                </div>
-              </div>
               <div
-                class="flex bg-red-500 px-4 border-l-4 border-gray-300 text-white rounded-tr-lg"
+                class="flex text-red-600 rounded-tr"
               >
                 <div class="my-auto mx-auto">
                   <svg
-                    class="h-6 w-6 fill-current"
+                    class="h-8 w-8 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                   >
