5925644aca5079b3e644d0b60e2133bc3cf134b8
jonathanFager <faaager@gmail.com>
2020-12-06 17:35:49 +0100

    Removed the working button, and added a more beautiful one. Needs to have a property similar to V-bind but that cannot be applied to a Div. Consider reading the status from Nats?

--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -1,17 +1,33 @@
 <template>
-  <label for="toggle_button">
-    <span v-if="isActive" class="toggle__label">On</span>
-    <span v-if="!isActive" class="toggle__label">Off</span>
+<div class="flex flex-row space-x-2 items-center">
+    <span>Manual</span>
+  <div
+    class="flex justify-between items-center"
+    @click="toggleActive = !toggleActive"
+    
+  >
+    
+    <div
+      class="w-16 h-10 flex items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out"
+      :class="{ 'bg-green-400': toggleActive }"
+    >
+      <div
+        class="bg-white w-8 h-8 rounded-full shadow-md transform duration-300 ease-in-out"
+        :class="{ 'translate-x-6': toggleActive }"
+      ></div>
+    
+    </div>
+  </div>
+  <span>Auto</span>
+  </div>
 
-    <input type="checkbox" id="toggle_button" v-model="checkedValue" />
-    <span class="toggle__switch"></span>
-  </label>
+  
 </template>
 <script>
 export default {
   data() {
     return {
-      currentState: false,
+      toggleActive: false,
     };
   },
 
@@ -23,61 +39,13 @@ export default {
     checkedValue: {
       get() {
         return this.defaultState;
-        // Här kan vi subscriba om controllern säger något annat?
+        // Subscribe to changes from Nats?
       },
       set(newValue) {
         this.currentState = newValue;
-        // Här kan vi skicka publish
+        // Publish to nats?
       },
     },
   },
 };
 </script>
-<style scoped>
-.toggle__button {
-  vertical-align: middle;
-  user-select: none;
-  cursor: pointer;
-}
-.toggle__button input[type="checkbox"] {
-  opacity: 0;
-  position: absolute;
-  width: 1px;
-  height: 1px;
-}
-.toggle__button .toggle__switch {
-  display: inline-block;
-  height: 12px;
-  border-radius: 6px;
-  width: 40px;
-  background: #bfcbd9;
-  box-shadow: inset 0 0 1px #bfcbd9;
-  position: relative;
-  margin-left: 10px;
-  transition: all 0.25s;
-}
-
-.toggle__button .toggle__switch::after,
-.toggle__button .toggle__switch::before {
-  content: "";
-  position: absolute;
-  display: block;
-  height: 18px;
-  width: 18px;
-  border-radius: 50%;
-  left: 0;
-  top: -3px;
-  transform: translateX(0);
-  transition: all 0.25s cubic-bezier(0.5, -0.6, 0.5, 1.6);
-}
-
-.toggle__button .toggle__switch::after {
-  background: #4d4d4d;
-  box-shadow: 0 0 1px #666;
-}
-.toggle__button .toggle__switch::before {
-  background: #4d4d4d;
-  box-shadow: 0 0 0 3px rgba(0, 0, 0, 0.1);
-  opacity: 0;
-}
-</style>
