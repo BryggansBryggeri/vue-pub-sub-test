0edcc0d0e15c69242ccbaad978eea3ecf16859b9
Jakob <jakob.lindqvist@chalmers.se>
2020-12-08 09:54:31 +0100

    Draft for generic Result type

---
+++ src/pub_sub.ts
@@ -0,0 +1,29 @@
+export default class Result<T> {
+  private ok: T | null = null;
+  private err: string | null = null;
+
+  public is_ok(): boolean {
+    return this.ok !== null;
+  }
+
+  public unwrap(): T | string {
+    if (this.is_ok()) {
+      return this.ok as T;
+    } else {
+      return this.err as string;
+    }
+  }
+
+  private constructor(ok: T | null, err: string | null) {
+    this.ok = ok;
+    this.err = err;
+  }
+
+  public static from_err(err: string) {
+    return new Result(null, err);
+  }
+
+  public static from_ok<T>(ok: T) {
+    return new Result(ok, null);
+  }
+}
