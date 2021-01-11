679bb09d9ec9ca00913a90719539e6ef3690540e
jonathanFager <faaager@gmail.com>
2021-01-11 13:36:47 +0100

    Loading spinner now in place, how to update?

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,41 @@ export default class App extends Vue {
   created(): void {
     eventbus.start();
   }
+
+  private loading = eventbus.ready();
+
 }
 </script>
 
 <template>
   <div id="app">
+    <div v-if="loading">
+      <div 
+      class="fixed inset-0 z-50 transition duration-150 ease-in-out flex justify-center items-center text-green-500"
+    >
+      <svg
+        class="animate-spin h-64 w-64"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
+    <div class="opacity-90 fixed inset-0 z-40 bg-black"></div>
+    </div>
     <router-view />
   </div>
 </template>
