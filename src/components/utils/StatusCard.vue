248f1446677e3c7549447101a787af20d0742217
jonathanFager <faaager@gmail.com>
2021-01-11 12:21:15 +0100

    Now working, but still errors in console

--- src/components/utils/StatusCard.vue
+++ src/components/utils/StatusCard.vue
@@ -2,11 +2,11 @@
   <div
     class="rounded-lg space-y-4 border-2 dark:bg-blue-gray-800 p-2 min-h-20 h-full flex flex-col"
     :class="{
-      'col-span-full': isFullWidth,
-      'col-span-1': !isFullWidth,
-      'bg-blue-gray-100 border-transparent': status = 1,
-      'bg-yellow-100 border-yellow-400': status = 2,
-      'bg-red-100 border-red-600': status = 3,
+      'col-span-full': fullWidth,
+      'col-span-1': !fullWidth,
+      'bg-blue-gray-100 border-transparent': isStatus === 1,
+      'bg-yellow-100 border-yellow-400': isStatus === 2,
+      'bg-red-100 border-red-600': isStatus === 3,
     }"
   >
     <slot></slot>
@@ -21,10 +21,14 @@ import { IndicatorType } from "@/utils";
   components: {
   },
 })
-export default class StatusInd extends Vue {
-  @Prop({ required: true }) status!: IndicatorType;
+export default class StatusCard extends Vue {
+  @Prop({ required: true }) status!: number;
 
-  @Prop({ default: false }) fullwidth!: boolean;
+  @Prop({ default: false }) fullWidth!: boolean;
+
+  get isStatus(): number {
+      return this.status
+  }
 
 }
 
