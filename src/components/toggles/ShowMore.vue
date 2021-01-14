77de5ea4e051acb3882ea93c675716db706b203a
jonathanFager <faaager@gmail.com>
2021-01-14 22:44:11 +0100

    showmore refactor

---
+++ src/components/toggles/ShowMore.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-row justify-center">
+    <svg-icon
+      class="transform transition opacity-60 duration-500 ease-in-out"
+      :class="{ '-rotate-90 ': showMore, 'rotate-90': !showMore }"
+      name="chevronRight"
+      size="4"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import SvgIcon from "@/components/symbols/SvgIcon.vue";
+
+@Component({
+  components: {
+      SvgIcon
+  },
+})
+export default class ShowMore extends Vue {
+    @Prop() isMore!: boolean
+    
+    get showMore(){
+        return this.isMore;
+    }
+}
+</script>
